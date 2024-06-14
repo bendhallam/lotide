bc6a873acc5175b108d04792a0de312c8b9e923c
Benjamin <bendhallam@gmail.com>
2024-06-14 11:34:43 -0600

    Implemented Head

---
+++ head.js
@@ -0,0 +1,20 @@
+// ASSERT EQUAL FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// FUNCTION IMPLEMENTATION
+const head = function(array) {
+  return array[0];
+};
+
+// TEST CASES
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), undefined);
