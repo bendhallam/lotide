c1aab85b9578a92cd056ad4e057df4c45a28a0b5
Benjamin <bendhallam@gmail.com>
2024-06-15 10:07:28 -0600

    improved assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -17,8 +17,10 @@ const eqArrays = function(arrOne, arrTwo) {
 const assertArraysEqual = function(actual, expected) {
   if (!eqArrays(actual, expected)) { // check if arrays aren't equal
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`); // if unequal, log failure message to console
+    return false;
   } else {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`); // if equal, log success message to console
+    return true;
   }
 };
 
