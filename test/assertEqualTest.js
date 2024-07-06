70830e2709124f1fd1bb6525e41f07fdc3b09320
Benjamin <bendhallam@gmail.com>
2024-07-05 19:23:19 -0600

    refactorized tests for head.js and assertEqual.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require("../assertEqual");
+
+
+//TEST CODE
+const babies = true;
+const apples = true;
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(babies, apples);
+assertEqual("ostriches", "peanuts");
+assertEqual("Animals", "Animals");
