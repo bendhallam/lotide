657bba84ea1cd5a09f16a660d8e8751f78a44178
Benjamin <bendhallam@gmail.com>
2024-07-05 19:53:04 -0600

    refactored assertArraysEqual.js to remove redefining of function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-// IMPLEMENT EQUAL ARRAYS FUNCTION
-
-const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) { // check if arrays are the different length
-    return false;
-  } else {
-    for (let i = 0; i < arrOne.length; i++)
-      if (arrOne[i] !== arrTwo[i]) { // check if arrays have any mismatching elements
-        return false;
-      }
-  }
-  return true; // if neither differences are flagged, return true
-};
-
+const eqArrays = require("./eqArrays");
 
 // IMPLEMENT ASSERT ARRAYS EQUAL FUNCTION
 const assertArraysEqual = function(actual, expected) {
